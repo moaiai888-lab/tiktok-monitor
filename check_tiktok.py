4af8533c3dd1bc87fe745e76e2158a4cc1b43c72
moaiai888-lab <moaiai888@gmail.com>
2026-05-30 13:55:39 +0700

    Update check_tiktok.py

--- check_tiktok.py
+++ check_tiktok.py
@@ -7,24 +7,31 @@ APIFY_TOKEN = os.environ["APIFY_TOKEN"]
 
 DATASET_ID = "Mgj9b1lgpnjVRa42Q"
 
-url = f"https://api.apify.com/v2/datasets/{DATASET_ID}/items?token={APIFY_TOKEN}"
+url = f"https://api.apify.com/v2/datasets/{DATASET_ID}/items?token={APIFY_TOKEN}&format=json&clean=true"
 
 data = requests.get(url).json()
+
+print(data)
+
+if isinstance(data, dict):
+    print("Data is dict, not list")
+    exit()
 
 if not data:
     print("No data")
     exit()
 
-latest_video = data[0]["id"]
+latest = data[0]
+latest_video = latest.get("id")
 
 with open("last_video.txt", "r") as f:
     old_video = f.read().strip()
 
 if latest_video != old_video:
+    profile = latest.get("authorMeta", {}).get("profileUrl", "https://www.tiktok.com/@mokjuri")
+    text = latest.get("text", "")
 
-    profile = data[0]["authorMeta"]["profileUrl"]
-
-    message = f"🎉 คลิปใหม่จาก TikTok\n\n{profile}"
+    message = f"🎉 คลิปใหม่จาก TikTok\n\nช่อง: @mokjuri\n{text}\n\n{profile}"
 
     requests.post(
         f"https://api.telegram.org/bot{TOKEN}/sendMessage",
@@ -38,6 +45,5 @@ if latest_video != old_video:
         f.write(latest_video)
 
     print("New video detected")
-
 else:
     print("No new video")
